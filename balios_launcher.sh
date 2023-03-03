96728b9782f4b5dd28ccde3e9c1991bb52d4e134
Recitasse <raphael.gougeonpro@gmail.com>
2023-03-03 01:02:11 +0100

    Ajout de la commande reload

--- balios_launcher.sh
+++ balios_launcher.sh
@@ -25,14 +25,17 @@ firefox -new-window http://dronebalios.com/
 echo -e "Vous pouvez afficher toutes les commandes avec la commande aide \n"
 echo -e "\033[33mLes commandes : \033[0m"
 echo -e "   \033[33mstop\033[0m: pour arrêter."
-echo -e "   \033[33mredm\033[0m: pour redémarrer.\n"
-echo -e "   \033[33mjoyinput\033[0m: pour afficher le nom de l'évènement.\n"
+echo -e "   \033[33mredm\033[0m: pour redémarrer."
+echo -e "   \033[33mraf\033[0m: pour rafraichir la configuration du server."
+echo -e "   \033[33mjoyinput\033[0m: pour afficher le nom de l'évènement."
+echo -e "   \033[33msql-session\033[0m: pour afficher les différentes sessions."
 
 # Lire une commande
 read -p "Votre commande : " cmd
 
 while [ "${cmd}" != "stop" ]
 do
+    #Pour redémarrer
     if [ "${cmd}" == "redm" ]
     then
        sudo systemctl restart mysql
@@ -41,6 +44,15 @@ do
        sudo systemctl status apache2
        echo -e "\033[32mMySQL et Apache ont redémarré correctement! \033[0m"
        
+    #Pour redémarrer
+    elif [ "${cmd}" == "raf" ]
+    then
+       sudo systemctl reload mysql
+       sudo systemctl reload apache2
+       sudo systemctl status mysql
+       sudo systemctl status apache2 
+    
+    #Pour lire l'event du joystik
     elif [ "${cmd}" == "joyinput" ]
     then
        result=$(sudo cat /proc/bus/input/devices | grep -B 2 -A 9 Controller)
@@ -50,6 +62,9 @@ do
        else
            echo -e "${result}\n"
        fi
+    #Pour redémarrer
+    
+
 
     else
        echo -e "\033[31mCette commande n'existe pas! \033[0m"
